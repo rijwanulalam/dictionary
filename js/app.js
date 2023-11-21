9cb03623498d404cecd43c748f616e281165e6d6
rijwanulalam <rijwanul007@gmail.com>
2023-11-22 00:03:19 +0600

    link fetched

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-const loadData = async () => {
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/hello`;
+const loadData = async (inputData) => {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputData}`;
     const res = await fetch(url);
     const data = await res.json();
     console.log(data[0])
 }
 
-
-
-loadData();
+document.getElementById("search-btn").addEventListener("click", function(){
+    const inputText = document.getElementById("input").value;
+    loadData(inputText)
+})
