64aad91ba136d1e79855da5558f6cc0ff3121eb7
rijwanulalam <rijwanul007@gmail.com>
2023-11-22 12:32:35 +0600

    pollished

--- js/app.js
+++ js/app.js
@@ -16,19 +16,19 @@ const showData = (word) => {
   const postDiv = document.createElement("div");
   postDiv.classList.add("p-10", "border", "border-sky-500", "rounded-lg");
   postDiv.innerHTML = `
-    <h2 class="text-6xl font-bold">${word.word}</h2>
-    <p class="text-blue-600/100 font-medium">${word.phonetic ? word.phonetic : word.phonetics[1].text}</p>
-    <p class="my-5 font-bold text-2xl font-medium">${word.meanings[0].partOfSpeech}</p>
-    <p class="my-2 text-blue-600/75 font-medium">Meaning</p>
+    <h2 class="text-6xl font-bold font-serif">${word.word}</h2>
+    <p class="text-2xl text-blue-600/100 font-medium">${word.phonetic ? word.phonetic : word.phonetics[1].text}</p>
+    <p class="my-5 font-bold text-2xl font-medium">${word.meanings[0].partOfSpeech} <hr></p>
+    <p class="my-2  font-medium">Meaning</p>
         <li class="list-disc font-medium">${word.meanings[0].definitions[0].definition}</li>
         <li class="list-disc font-medium">${word.meanings[0].definitions[1].definition}</li>
         <li class="list-disc font-medium">${word.meanings[0].definitions[2].definition}</li>
-    <h4 class="my-5 font-medium">Synonyms : ${word.meanings[0].synonyms[0] ? word.meanings[0].synonyms[0] : word.word}</h4>
-    <p class="my-5 font-bold text-2xl font-medium">${word.meanings[1].partOfSpeech}</p>
+    <h4 class="my-5 font-medium">Synonyms : <span class="text-blue-600/100">${word.meanings[0].synonyms[0] ? word.meanings[0].synonyms[0] : word.word}</span></h4>
+    <p class="my-5 font-bold text-2xl font-medium">${word.meanings[1].partOfSpeech} <hr></p>
     <p class="my-2 font-medium">Meaning</p>
         <li class="list-disc font-medium">${word.meanings[1].definitions[0].definition}</li>
         <li class="list-none font-medium text-blue-600/75">${word.meanings[1].definitions[0].example ? word.meanings[1].definitions[0].example : '"make a example by your own"'}</li>
-    <p class="my-5 font-medium">Source : <span class="text-blue-600/75">${word.sourceUrls[0]}</span></p>
+    <p class="my-5 font-medium">Source : <span class="text-blue-600/75 underline underline-offset-1">${word.sourceUrls[0]}</span></p>
   `;
   wordContainer.appendChild(postDiv);
 };
