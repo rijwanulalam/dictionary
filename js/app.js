457cf63566c729f77e0f53c52389ef56763cfc49
rijwanulalam <rijwanul007@gmail.com>
2023-11-22 14:21:47 +0600

    audio added

--- js/app.js
+++ js/app.js
@@ -6,29 +6,61 @@ const loadData = async (inputData) => {
     // console.log(data);
     showData(data[0]);
   } catch (err) {
-    alert("can not find word")
-    window.location.reload();
+    // alert("can not find word");
+    // window.location.reload();
+    // console.log(err)
   }
 };
 
 const showData = (word) => {
+    console.log(word)
   const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
   const postDiv = document.createElement("div");
   postDiv.classList.add("p-10", "border", "border-sky-500", "rounded-lg");
   postDiv.innerHTML = `
-    <h2 class="text-6xl font-bold font-serif">${word.word}</h2>
-    <p class="text-2xl text-blue-600/100 font-medium">${word.phonetic ? word.phonetic : word.phonetics[1].text}</p>
-    <p class="my-5 font-bold text-2xl font-medium">${word.meanings[0].partOfSpeech} <hr></p>
+    <div class="flex items-center justify-between">
+        <div>
+            <h2 class="text-6xl font-bold font-serif">${word.word}</h2>
+            <p class="text-2xl text-blue-600/100 font-medium">${
+            word.phonetic ? word.phonetic : word.phonetics[1].text
+            }</p>
+        </div>
+        <div>
+            <audio src="${word.phonetics[0].audio || word.phonetics[1].audio || word.phonetics[2].audio}" type="audio/mp3" controls></audio>
+        </div>
+    </div>
+    <p class="my-5 font-bold text-2xl font-medium">${
+      word.meanings[0].partOfSpeech
+    } <hr></p>
     <p class="my-2  font-medium">Meaning</p>
-        <li class="list-disc font-medium">${word.meanings[0].definitions[0].definition}</li>
-        <li class="list-disc font-medium">${word.meanings[0].definitions[1].definition}</li>
-        <li class="list-disc font-medium">${word.meanings[0].definitions[2].definition}</li>
-    <h4 class="my-5 font-medium">Synonyms : <span class="text-blue-600/100">${word.meanings[0].synonyms[0] ? word.meanings[0].synonyms[0] : word.word}</span></h4>
-    <p class="my-5 font-bold text-2xl font-medium">${word.meanings[1].partOfSpeech} <hr></p>
+        <li class="list-disc font-medium">${
+          word.meanings[0].definitions[0].definition
+        }</li>
+        <li class="list-disc font-medium">${
+          word.meanings[0].definitions[1].definition ? word.meanings[0].definitions[1].definition : "this is most commonly used word"
+        }</li>
+        <li class="list-disc font-medium">${
+          word.meanings[0].definitions[2].definition ? word.meanings[0].definitions[2].definition : "old english word"
+        }</li>
+    <h4 class="my-5 font-medium">Synonyms : <span class="text-blue-600/100">${
+      word.meanings[0].synonyms[0] ? word.meanings[0].synonyms[0] : word.word
+    }</span></h4>
+    <p class="my-5 font-bold text-2xl font-medium">${
+      word.meanings[1].partOfSpeech
+    } <hr></p>
     <p class="my-2 font-medium">Meaning</p>
-        <li class="list-disc font-medium">${word.meanings[1].definitions[0].definition}</li>
-        <li class="list-none font-medium text-blue-600/75">${word.meanings[1].definitions[0].example ? word.meanings[1].definitions[0].example : '"make a example by your own"'}</li>
-    <p class="my-5 font-medium">Source : <span class="text-blue-600/75 underline underline-offset-1">${word.sourceUrls[0]}</span></p>
+        <li class="list-disc font-medium">${
+          word.meanings[1].definitions[0].definition
+        }</li>
+        <li class="list-none font-medium text-blue-600/75">${
+          word.meanings[1].definitions[0].example
+            ? word.meanings[1].definitions[0].example
+            : '"make a example by your own"'
+        }</li>
+    <p class="my-5 font-medium">Source : <span class="text-blue-600/75 underline underline-offset-1">${
+      word.sourceUrls[0]
+    }</span></p>
   `;
   wordContainer.appendChild(postDiv);
 };
